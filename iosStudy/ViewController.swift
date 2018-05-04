37c94fe1b02fcdc8f46027e5f6f28d9765043a49
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-04 16:46:36 -0700

    bounds and center

--- iosStudy/ViewController.swift
+++ iosStudy/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-    
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -16,14 +16,14 @@ class ViewController: UIViewController {
         let mainview = self.view! //you add this
         let v1 = UIView(frame:CGRect(x: 113, y: 111, width: 132, height: 194))
         v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
-        let v2 = UIView(frame: CGRect(x: 41, y: 56, width: 132, height: 194))
+        let v2 = UIView(frame: v1.bounds.insetBy(dx: 10, dy: 10))
         v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
-        let v3 = UIView(frame: CGRect(x: 43, y: 197, width: 160, height: 230))
-        v3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        //let v3 = UIView(frame: CGRect(x: 43, y: 197, width: 160, height: 230))
+        //v3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
         
         mainview.addSubview(v1)
         v1.addSubview(v2)
-        mainview.addSubview(v3)
+        //mainview.addSubview(v3)
     }
 
     override func didReceiveMemoryWarning() {
