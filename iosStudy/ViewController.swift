4375b468a1303c61684c688cadba85fe29831d4d
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-04 16:51:49 -0700

    the superview's bounds origin has been shifted

--- iosStudy/ViewController.swift
+++ iosStudy/ViewController.swift
@@ -18,12 +18,11 @@ class ViewController: UIViewController {
         v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
         let v2 = UIView(frame: v1.bounds.insetBy(dx: 10, dy: 10))
         v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
-        //let v3 = UIView(frame: CGRect(x: 43, y: 197, width: 160, height: 230))
-        //v3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
         
         mainview.addSubview(v1)
         v1.addSubview(v2)
-        //mainview.addSubview(v3)
+        v1.bounds.origin.x += 20
+        v1.bounds.origin.y += 20
     }
 
     override func didReceiveMemoryWarning() {
