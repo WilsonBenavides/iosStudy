4e426d03fb2afa45f8e3ce23014044b5eaf14cfd
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-04 16:44:14 -0700

    stack of views

--- iosStudy/ViewController.swift
+++ iosStudy/ViewController.swift
@@ -7,17 +7,23 @@
 //
 
 import UIKit
-
+    
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let mainview = self.view! //you add this
-        //my code starts here:
-        let v = UIView(frame: CGRect(x: 100, y: 100, width: 50, height: 50))
-        v.backgroundColor = .red //small red square
-        mainview.addSubview(v)
+        let v1 = UIView(frame:CGRect(x: 113, y: 111, width: 132, height: 194))
+        v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
+        let v2 = UIView(frame: CGRect(x: 41, y: 56, width: 132, height: 194))
+        v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
+        let v3 = UIView(frame: CGRect(x: 43, y: 197, width: 160, height: 230))
+        v3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        
+        mainview.addSubview(v1)
+        v1.addSubview(v2)
+        mainview.addSubview(v3)
     }
 
     override func didReceiveMemoryWarning() {
