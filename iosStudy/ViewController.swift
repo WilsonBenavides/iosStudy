23627f4de8b9313daae3bd6bedf29f30ae2df7eb
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-05 06:11:27 -0700

    alternate sets of views and constraints

--- iosStudy/ViewController.swift
+++ iosStudy/ViewController.swift
@@ -9,32 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var v1 : UIView!
+    var v2 : UIView!
+    var v3 : UIView!
+    
+    var constraintsWith = [NSLayoutConstraint]()
+    var constraintsWithout = [NSLayoutConstraint]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let mainview = self.view!
         
-        let v1 = UIView(frame: CGRect(x: 100, y: 111, width: 132, height: 194))
-        v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
+        
+        let v1 = UIView()
+        v1.backgroundColor = .red
+        v1.translatesAutoresizingMaskIntoConstraints = false
+        
         let v2 = UIView()
-        v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
-        let v3 = UIView()
-        v3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
-        
-        mainview.addSubview(v1)
-        v1.addSubview(v2)
-        v2.addSubview(v3)
+        v2.backgroundColor = .yellow
         v2.translatesAutoresizingMaskIntoConstraints = false
+        
+        let v3 = UIView()
+        v3.backgroundColor = .blue
         v3.translatesAutoresizingMaskIntoConstraints = false
+
+        mainview.addSubview(v1)
+        mainview.addSubview(v2)
+        mainview.addSubview(v3)
         
-        let d = ["v2": v2, "v3": v3]
-        NSLayoutConstraint.activate([
-            NSLayoutConstraint.constraints(withVisualFormat: "H:|[v2]|", options: [], metrics: nil, views: d),
-            NSLayoutConstraint.constraints(withVisualFormat: "V:|[v2(10)]", options: [], metrics: nil, views: d),
-            NSLayoutConstraint.constraints(withVisualFormat: "H:[v3(20)]|", options: [], metrics: nil, views: d),
-            NSLayoutConstraint.constraints(withVisualFormat: "V:[v3(20)]|", options: [], metrics: nil, views: d)
-            ].flatMap{$0})
+        self.v1 = v1
+        self.v2 = v2
+        self.v3 = v3
         
+        //construct constraints
+        let c1 = NSLayoutConstraint.constraints(withVisualFormat:"H:|-(20)-[v(100)]", metrics: nil, views: ["v":v1])
+        let c2 = NSLayoutConstraint.constraints(withVisualFormat:"H:|-(20)-[v(100)]", metrics: nil, views: ["v":v2])
+        let c3 = NSLayoutConstraint.constraints(withVisualFormat:"H:|-(20)-[v(100)]", metrics: nil, views: ["v":v3])
+        let c4 = NSLayoutConstraint.constraints(withVisualFormat:"V:|-(100)-[v(20)]", metrics: nil, views: ["v":v1])
+        let c5with = NSLayoutConstraint.constraints(withVisualFormat:"V:[v1]-(20)-[v2(20)]-(20)-[v3(20)]", metrics: nil, views: ["v1":v1, "v2":v2, "v3":v3])
+        let c5without = NSLayoutConstraint.constraints(withVisualFormat:"V:[v1]-(20)-[v3(20)]", metrics: nil, views: ["v1":v1, "v3":v3])
+        
+        //first set of constraints
+        self.constraintsWith.append(contentsOf:c1)
+        self.constraintsWith.append(contentsOf:c2)
+        self.constraintsWith.append(contentsOf:c3)
+        self.constraintsWith.append(contentsOf:c4)
+        self.constraintsWith.append(contentsOf:c5with)
+        // second set of constraints
+        self.constraintsWithout.append(contentsOf:c1)
+        self.constraintsWithout.append(contentsOf:c3)
+        self.constraintsWithout.append(contentsOf:c4)
+        self.constraintsWithout.append(contentsOf:c5without)
+        
+        // apply first set
+        NSLayoutConstraint.activate(self.constraintsWith)
+        
+        if self.v2.superview != nil {
+            self.v2.removeFromSuperview()
+            NSLayoutConstraint.deactivate(self.constraintsWith)
+            NSLayoutConstraint.activate(self.constraintsWithout)
+        } else {
+            mainview.addSubview(v2)
+            NSLayoutConstraint.deactivate(self.constraintsWithout)
+            NSLayoutConstraint.activate(self.constraintsWith)
+        }
     }
 
     override func didReceiveMemoryWarning() {
